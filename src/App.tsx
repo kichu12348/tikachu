e221d983d46e7ba6e44abcb38913742b440ab64b
Mahadevan Reji <rmahadevan574@gmail.com>
2025-12-03 11:32:04 +0530

    Add image loading logic in App component for character animations

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ console.clear();
 
 function App():React.JSX.Element {
 
+  React.useEffect(()=>{
+    const img = new Image();
+    img.src = '/actors/run_1.webp';
+    img.onload = () => {
+      console.log('run 1 loaded');
+    }
+    img.src = '/actors/run_2.webp';
+    img.onload=()=>{
+      console.log('run 2 loaded');
+    }
+  },[])
+
   return (
     <div className="App">
       <Game />
